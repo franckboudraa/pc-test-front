7a49e373f14cc331562db7034279d0fa124c03bc
neocydfr <franckboudraa@gmail.com>
2018-03-16 12:46:35 +0100

    working simple table with sorting

--- src/Dishes/DishesItem.js
+++ src/Dishes/DishesItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Table } from 'semantic-ui-react';
 
-const DishesItem = () => {
-  return <div>DishesItem</div>;
+const DishesItem = props => {
+  return (
+    <Table.Row key={props.id}>
+      <Table.Cell>{props.id}</Table.Cell>
+      <Table.Cell>{props.title}</Table.Cell>
+      <Table.Cell>{props.rating}</Table.Cell>
+    </Table.Row>
+  );
 };
 
 export default DishesItem;
--- src/Dishes/DishesList.js
+++ src/Dishes/DishesList.js
@@ -1,26 +1,93 @@
 import React, { PureComponent } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchDishes } from '../actions';
 
 import DishesItem from './DishesItem';
 import DishesError from './DishesError';
 
-import { Loader } from 'semantic-ui-react';
+import { Loader, Table } from 'semantic-ui-react';
 
 class DishesList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      column: null,
+      direction: null,
+      data: []
+    };
+  }
+
   componentDidMount() {
     this.props.fetchDishes();
+    /*this.setState({
+      data: this.props.dishes.dishes
+    });*/
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      data: nextProps.dishes.dishes
+    });
+  }
+
+  handleSort = clickedColumn => () => {
+    const { column, direction, data } = this.state;
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending'
+      });
+
+      return;
+    }
+
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending'
+    });
+  };
+
   render() {
-    console.log(this.props);
-    const { error, loading, dishes } = this.props.dishes;
+    const { error, loading } = this.props.dishes;
+    const { column, data, direction } = this.state;
     return (
       <div>
         {loading && <Loader active inline="centered" />}
         {error && <DishesError />}
         {!error &&
           !loading &&
-          dishes.map(dish => <DishesItem key={dish.id} {...dish} />)}
+          data && (
+            <Table sortable celled fixed>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell
+                    sorted={column === 'id' ? direction : null}
+                    onClick={this.handleSort('id')}
+                  >
+                    id
+                  </Table.HeaderCell>
+                  <Table.HeaderCell
+                    sorted={column === 'title' ? direction : null}
+                    onClick={this.handleSort('title')}
+                  >
+                    Title
+                  </Table.HeaderCell>
+                  <Table.HeaderCell
+                    sorted={column === 'rating' ? direction : null}
+                    onClick={this.handleSort('rating')}
+                  >
+                    Rating
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {data.map(dish => <DishesItem key={dish.id} {...dish} />)}
+              </Table.Body>
+            </Table>
+          )}
       </div>
     );
   }
